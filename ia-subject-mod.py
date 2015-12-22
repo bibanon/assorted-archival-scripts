28d6e8e5c11de127c9e5758b98fbb5dada12aeaa
Antonizoon <antonizoon@bibanon.org>
2015-12-21 18:01:22 -0800

    Removed semicolon adder

--- ia-subject-mod.py
+++ ia-subject-mod.py
@@ -21,10 +21,7 @@ item = get_item(identifier)
 subject = item.metadata['subject']
 
 # append new subject to existing subject
-if subject.endswith(';'):
-	new_subject = subject + add_subject
-else:
-	new_subject = subject + ';' + add_subject
+new_subject = str(subject) + add_subject
 
 # upload new metadata
 r = item.modify_metadata(dict(subject=new_subject))
